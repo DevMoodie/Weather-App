e92f50f8db6ce676abdc45866e42509f69c3b3ab
DevMoodie <Mahmoudyf96@gmail.com>
2024-08-23 11:52:53 -0400

    Build WeatherView for user input and display

---
+++ Weather App/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  Weather App
+//
+//  Created by Moody on 2024-08-23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
